137485b10c6f0637de373c12d272216cc1c919d0
Rafal Jasinski <1jasinskirafal@gmail.com>
2020-09-09 20:05:38 +0200

    Code refactoring

--- include/Database.h
+++ include/Database.h
@@ -12,7 +12,7 @@
 #define MAX_RECORDS_COUNT 16
 #define SUCCESS 1
 #define ERROR -1
-#define NEW_DATABASE 0
+#define NEW_DATABASE 2
 
 #define RECORDS_DIFFERENT 0
 #define RECORDS_SAME 1
@@ -39,36 +39,22 @@ extern const char* SHARED_CONDITION_NAMETAG;
 extern const char* REMOTE_DATABASE;
 
 /*
+*   Database setup - crucial for the .so file to work properly.
+*   
+*   Example:
+*            Database_INIT(local_db_ptr);
+*/
+int Database_INIT(Database*, pthread_t*, pthread_attr_t*);
+
+/*
 *   Main functionality of a Database:
 *   - Update the local database with remote file contents
 *   - Write the local database to a remote file
 *   - Change a single record in the local database
 */
 
-int Read_Database_From_File(FILE*, Database*);
-int Write_Database_To_File(Database*, FILE*);
+int Read_Database_From_File(Database*);
+int Write_Database_To_File(Database*);
 int Change_Local_Record(Database*, const __uint8_t, const Record);
-
-/*
-*   Database setup - crucial for the .so file to work properly.
-*   
-*   Example:
-*       if (Database_JOIN(local_db_ptr) == -1){
-*            Database_INIT(local_db_ptr)
-*       }
-*/
-int Database_INIT(Database*);
-int Database_JOIN(Database*);
-
-/*
-*   These functions are used for the synchronization mechanism,
-*   Dedicate a single thread that is going to be woken up each time a remote database gets changes
-*   
-*   Example:
-*       pthread_create(Synchronization_Thread, NULL, Synchronize_With_Remote_Task, local_DB_ptr);
-*/
-
-void* Synchronize_With_Remote_Task(void *);
-void* Write_To_Database_With_Notify(void*);
 
 #endif
--- src/Database.c
+++ src/Database.c
@@ -18,7 +18,89 @@ Sync_State Changed_Records_Report;
                             FUNCTIONS:
 ///////////////////////////////////////////////////////////////////////////////*/
 
-inline int Read_Database_From_File(FILE* Database_FD, Database* local_db){
+static int Initialize_Shared_Memory();
+static int Initialize_Thread_Synchronization();
+static int Initialize_Remote_Database(Database*);
+static void* Pthread_Synchronize_With_Remote(void *);
+
+static inline int Compare_Records(Database*, Database*);
+static inline int Remote_DB_Comparison_Check(Database*);
+static void Synchronize_Local_DB(FILE*, Database*, Sync_State*);
+
+int Database_INIT(Database* local_db, pthread_t* Synchronization_Thread, pthread_attr_t* Synchronization_Attributes){
+    
+    if (Initialize_Shared_Memory() == ERROR){
+        perror("Shared memory initialization failed");
+        return ERROR;
+    }
+    
+    if (Initialize_Thread_Synchronization() == ERROR){
+        perror("Thread synchronization initialization failed");
+        return ERROR;
+    }
+
+    if (Initialize_Remote_Database(local_db) == ERROR){
+        perror("Remote Database initialization failed");
+        return ERROR;
+    }
+
+    if (pthread_create(Synchronization_Thread,Synchronization_Attributes,Pthread_Synchronize_With_Remote,local_db) != 0){
+        perror("Synchronization thread initialization failed");
+        return ERROR;
+    }
+    return SUCCESS;
+}
+
+static int Initialize_Shared_Memory(){
+    int Shared_Mutex_FD;
+    int Shared_Condition_FD;
+
+    Shared_Mutex_FD = shm_open(SHARED_MUTEX_NAMETAG, O_RDWR, 0666);
+    if (Shared_Mutex_FD == -1) Shared_Mutex_FD = shm_open(SHARED_MUTEX_NAMETAG, O_CREAT|O_RDWR, 0666);
+    if (Shared_Mutex_FD == -1) return ERROR;
+    ftruncate(Shared_Mutex_FD,sizeof(pthread_mutex_t));
+    Database_Synchronization_Lock = mmap(0, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Mutex_FD, 0);
+
+
+    Shared_Condition_FD = shm_open(SHARED_CONDITION_NAMETAG, O_RDWR, 0666);
+    if (Shared_Condition_FD == -1) Shared_Condition_FD = shm_open(SHARED_CONDITION_NAMETAG, O_CREAT|O_RDWR, 0666);
+    if (Shared_Condition_FD == -1) return ERROR;
+    ftruncate(Shared_Condition_FD, sizeof(pthread_cond_t));
+    Database_Changed_Signal = mmap(0, sizeof(pthread_cond_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Condition_FD, 0);
+    return SUCCESS;
+}
+
+static int Initialize_Thread_Synchronization(){
+    pthread_mutexattr_t Sync_Mutex_Attr;
+    pthread_condattr_t Sync_Cond_Attr;
+
+    //Initialize a shared memory mutex
+    if (pthread_mutexattr_init(&Sync_Mutex_Attr) != 0) return ERROR;
+    pthread_mutexattr_setpshared(&Sync_Mutex_Attr,PTHREAD_PROCESS_SHARED);
+    if (pthread_mutex_init(Database_Synchronization_Lock, &Sync_Mutex_Attr) != 0) return ERROR;
+    pthread_mutexattr_destroy(&Sync_Mutex_Attr);
+
+    //Initialize a shared memory condition variable
+    if (pthread_condattr_init(&Sync_Cond_Attr) != 0) return ERROR;
+    pthread_condattr_setpshared(&Sync_Cond_Attr,PTHREAD_PROCESS_SHARED);
+    if (pthread_cond_init(Database_Changed_Signal, &Sync_Cond_Attr) != 0) return ERROR;
+    pthread_condattr_destroy(&Sync_Cond_Attr);
+
+    return SUCCESS;
+}
+
+static int Initialize_Remote_Database(Database* local_db){
+    if (Read_Database_From_File(local_db) == ERROR){
+        for (int i = 0; i<MAX_RECORDS_COUNT; ++i){
+            local_db->Records[i].Data = 0;
+        }
+        Write_Database_To_File(local_db);
+    }
+    return SUCCESS;
+}
+
+int Read_Database_From_File(Database* local_db){
+    FILE* Database_FD;
     int Operation_State = ERROR;
 
     pthread_mutex_lock(&Database_Access_Lock);
@@ -33,7 +115,8 @@ inline int Read_Database_From_File(FILE* Database_FD, Database* local_db){
     return Operation_State;
 }
 
-inline int Write_Database_To_File(Database* local_db, FILE* Database_FD){
+int Write_Database_To_File(Database* local_db){
+    FILE* Database_FD;
     int Operation_State = ERROR;
 
     pthread_mutex_lock(&Database_Access_Lock);
@@ -57,74 +140,22 @@ int Change_Local_Record(Database* local_db, const __uint8_t recordID, const Reco
     return ERROR;
 }
 
-int Database_INIT(Database* local_db){
+static void *Pthread_Synchronize_With_Remote(void * Database_input){
     FILE* Database_FD = NULL;
-    int Shared_Mutex_FD;
-    int Shared_Condition_FD;
+    Database Remote_Database_Comparison_Copy;
+    while(1){
+        Read_Database_From_File(&Remote_Database_Comparison_Copy);
 
-    //Initialize sharem memory fragments to be available for all processes.
-    Shared_Mutex_FD = shm_open(SHARED_MUTEX_NAMETAG, O_CREAT|O_RDWR, 0666);
-    if (Shared_Mutex_FD == -1) return ERROR;
-    ftruncate(Shared_Mutex_FD,sizeof(pthread_mutex_t));
-    Database_Synchronization_Lock = mmap(0, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Mutex_FD, 0);
-
-    Shared_Condition_FD = shm_open(SHARED_CONDITION_NAMETAG, O_CREAT|O_RDWR, 0666);
-    if (Shared_Condition_FD == -1) return ERROR;
-    ftruncate(Shared_Condition_FD, sizeof(pthread_cond_t));
-    Database_Changed_Signal = mmap(0, sizeof(pthread_cond_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Condition_FD, 0);
-
-    //Initialize Database Synchronization related mutex and condition.
-    pthread_mutexattr_t Sync_Mutex_Attr;
-    pthread_mutexattr_init(&Sync_Mutex_Attr);
-    pthread_mutexattr_setpshared(&Sync_Mutex_Attr,PTHREAD_PROCESS_SHARED);
-    pthread_mutex_init(Database_Synchronization_Lock, &Sync_Mutex_Attr);
-    pthread_mutexattr_destroy(&Sync_Mutex_Attr);
-
-    pthread_condattr_t Sync_Cond_Attr;
-    pthread_condattr_init(&Sync_Cond_Attr);
-    pthread_condattr_setpshared(&Sync_Cond_Attr,PTHREAD_PROCESS_SHARED);
-    pthread_cond_init(Database_Changed_Signal, &Sync_Cond_Attr);
-    pthread_condattr_destroy(&Sync_Cond_Attr);
-
-    //Initialize the local datase before the lock in order to limit the delay of a main thread
-    if (Read_Database_From_File(Database_FD,local_db) < 1){
-        for (int i = 0; i<MAX_RECORDS_COUNT; ++i){
-            local_db->Records[i].Data = 0;
+        pthread_mutex_lock(Database_Synchronization_Lock);
+        while(Remote_DB_Comparison_Check((Database*)Database_input)){
+            pthread_cond_wait(Database_Changed_Signal, Database_Synchronization_Lock);
         }
-        Write_Database_To_File(local_db,Database_FD);
-        return NEW_DATABASE;
+        Synchronize_Local_DB(Database_FD, (Database*)Database_input, &Changed_Records_Report);
+        pthread_mutex_unlock(Database_Synchronization_Lock);
     }
-    return SUCCESS;
+    return NULL;
 }
-
-int Database_JOIN(Database* local_db){
-    FILE* Database_FD = NULL;
-    int Shared_Mutex_FD;
-    int Shared_Condition_FD;
-
-    Shared_Mutex_FD = shm_open(SHARED_MUTEX_NAMETAG, O_RDWR, 0666);
-    if (Shared_Mutex_FD == -1) return ERROR;
-    ftruncate(Shared_Mutex_FD,sizeof(pthread_mutex_t));
-    Database_Synchronization_Lock = mmap(0, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Mutex_FD, 0);
-
-
-    Shared_Condition_FD = shm_open(SHARED_CONDITION_NAMETAG, O_RDWR, 0666);
-    if (Shared_Condition_FD == -1) return ERROR;
-    ftruncate(Shared_Condition_FD, sizeof(pthread_cond_t));
-    Database_Changed_Signal = mmap(0, sizeof(pthread_cond_t), PROT_READ|PROT_WRITE, MAP_SHARED, Shared_Condition_FD, 0);
-
-    //Initialize the local datase before the lock in order to limit the delay of a main thread
-    if (Read_Database_From_File(Database_FD,local_db) < 1){
-        for (int i = 0; i<MAX_RECORDS_COUNT; ++i){
-            local_db->Records[i].Data = 0;
-        }
-        Write_Database_To_File(local_db,Database_FD);
-        return NEW_DATABASE;
-    }
-    return SUCCESS;
-}
-
-static int Compare_Records(Database* First, Database* Second){
+static inline int Compare_Records(Database* First, Database* Second){
     for(int i =0; i<MAX_RECORDS_COUNT; ++i){
         if (memcmp((void*)&First->Records[i], (void*)&Second->Records[i],sizeof(Record)) != 0)
             return RECORDS_DIFFERENT;
@@ -132,17 +163,16 @@ static int Compare_Records(Database* First, Database* Second){
     return RECORDS_SAME;
 }
 
-static int Remote_DB_Comparison_Check(Database* Database_input){
-    FILE* Database_FD = NULL;
-    Database temp;
-    Read_Database_From_File(Database_FD, &temp);
+static inline int Remote_DB_Comparison_Check(Database* Database_input){
+    Database Remote_Database_Comparison_Copy;
+    Read_Database_From_File(&Remote_Database_Comparison_Copy);
 
-    return Compare_Records(Database_input,&temp);
+    return Compare_Records(Database_input,&Remote_Database_Comparison_Copy);
 }
 
 static void Synchronize_Local_DB(FILE* Database_FD, Database* local_db, Sync_State* Sync_Report){
     Database Compared_DB;
-    Read_Database_From_File(Database_FD, &Compared_DB);
+    Read_Database_From_File(&Compared_DB);
 
     for(int i =0; i<MAX_RECORDS_COUNT; ++i){
         if (memcmp((void*)&Compared_DB.Records[i], (void*)&local_db->Records[i], sizeof(Record)) != 0){
@@ -153,26 +183,4 @@ static void Synchronize_Local_DB(FILE* Database_FD, Database* local_db, Sync_Sta
             Sync_Report->ChangedRecord[i] = false;
         }
     }
-}
-
-void* Synchronize_With_Remote_Task(void * Database_input){
-    FILE* Database_FD = NULL;
-    Database temp;
-    while(1){
-        Read_Database_From_File(Database_FD, &temp);
-
-        pthread_mutex_lock(Database_Synchronization_Lock);
-        while(Remote_DB_Comparison_Check((Database*)Database_input)){
-            pthread_cond_wait(Database_Changed_Signal, Database_Synchronization_Lock);
-        }
-        Synchronize_Local_DB(Database_FD, (Database*)Database_input, &Changed_Records_Report);
-        pthread_mutex_unlock(Database_Synchronization_Lock);
-    }
-}
-
-void* Write_To_Database_With_Notify(void* Database_input){
-    FILE* Database_FD = NULL;
-
-    Write_Database_To_File((Database*)Database_input,Database_FD);
-    return NULL;
 }
